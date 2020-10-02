9923659a2770c9656d1afc8b7736dd3d3b7f504c
alex4o <mrabonin@gmail.com>
2020-10-02 16:21:21 +0100

    Ran the clang format tool, and removed comented out code.

--- setjmp.c
+++ setjmp.c
@@ -6,16 +6,11 @@
 
 int main()
 {
-
 	jmp_buf buffer;
 	int res = setjmp(buffer);
-	//printf("setjmp return: %d\n", res);
 
 	uint32_t length = cheri_length_get(buffer);
-	//inspect_pointer(buffer);
-	//printf("len: %d\n", length);
-
-
+	
 	// buffer[0] == _JB_MAGIC_SETJMP == 0xbe87fd8a2910af01
 	// buffer[1] == $csp
 	// buffer[2] == $cfp
@@ -25,12 +20,13 @@ int main()
 	{
 		if (cheri_is_valid(((void **)buffer)[idx]))
 		{
-			void* csp = cheri_csp_get();
+			void *csp = cheri_csp_get();
 			uint64_t base = cheri_base_get(csp);
 			uint64_t length = cheri_length_get(csp);
-			uint64_t address = cheri_address_get(((void**)buffer)[idx]);
+			uint64_t address = cheri_address_get(((void **)buffer)[idx]);
 
-			if(address >= base && address <= (base + length)) {
+			if (address >= base && address <= (base + length))
+			{
 				printf("[STACK POINTER] ");
 			}
 			inspect_pointer(((void **)buffer)[idx]);
