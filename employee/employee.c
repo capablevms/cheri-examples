1b19b665953812259fb971550ea3b7cf9c9f9b77
Giuseppe De Ruvo <drgdr@pm.me>
2021-08-19 09:59:35 +0000

    Fix bug where stack pointer is returned to caller
    The tests did not detect a subtle bug due to `read_only` originally returning a
    stack pointer and causing undefined behaviour.

--- employee/employee.c
+++ employee/employee.c
@@ -15,9 +15,9 @@ void change_salary(struct employee *e, double salary)
 
 void print_details(struct employee *e)
 {
-	printf("Employee Id: %d \n", e->id);
+	printf("Employee Id: %d\n", e->id);
 	printf("Employee Name: %s \n", e->name);
 	printf("Employee Surname: %s\n", e->surname);
-	printf("Employee Salary: %lf\n", e->salary);
+	printf("Employee Salary: %.2lf\n", e->salary);
 	fflush(stdout);
 }
--- employee/read_only.c
+++ employee/read_only.c
@@ -1,29 +1,31 @@
 /* This program is very similar to "full_privileges.c"
  * but here we set the permissions of the capability
  * to LOAD, i.e. read-only. So the user is not allowed to
- * change the salary.
+ * change, e.g. the salary.
  */
 
 #include "../include/common.h"
 #include "include/employee.h"
+#include <assert.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 int main()
 {
-	struct employee employee = {0, "Good", "Employee", 55000};
-	double salary = 0.0;
+	struct employee employee = {1000, "Good", "Employee", 55000.0};
+	printf("--- EMPLOYEE ---\n");
 	// set the permissions to read-only
 	struct employee *ro_employee = set_read_only(&employee);
-	inspect_pointer(ro_employee);
+	assert((cheri_perms_get(ro_employee) & CHERI_PERM_STORE) == 0);
 	print_details(ro_employee);
-	printf("The struct is read-only so trying to change the salary will make the program crash...");
-	printf("\nInsert the new salary for this employee: \n");
+	double new_salary = 65000.0;
+	printf(
+		"\nThe struct is read-only so trying to change the salary to %.2lf will make the program "
+		"crash...\n",
+		new_salary);
 	fflush(stdout);
-	scanf("%lf", &salary);
-
-	change_salary(ro_employee, salary);
+	change_salary(ro_employee, new_salary);
 	print_details(ro_employee);
 }
 
@@ -32,11 +34,7 @@ int main()
  */
 struct employee *set_read_only(struct employee *e)
 {
-	char *ro_name = (char *)cheri_perms_and(e->name, CHERI_PERM_LOAD);
-	char *ro_surname = (char *)cheri_perms_and(e->surname, CHERI_PERM_LOAD);
-	struct employee *ro_employee =
-		(struct employee *)cheri_perms_and(e, CHERI_PERM_LOAD || CHERI_PERM_LOAD_CAP);
-	ro_employee->name = ro_name;
-	ro_employee->surname = ro_surname;
-	return ro_employee;
+	e->name = (char *)cheri_perms_and(e->name, CHERI_PERM_LOAD);
+	e->surname = (char *)cheri_perms_and(e->surname, CHERI_PERM_LOAD);
+	return (struct employee *)cheri_perms_and(e, CHERI_PERM_LOAD | CHERI_PERM_LOAD_CAP);
 }
