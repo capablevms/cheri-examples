d2ac0309d3eff27f3237f8d56d0f4ce6aff3b480
Jacob Bramley <jacob.bramley@arm.com>
2021-09-17 12:10:08 +0100

    Update buildbot scripts to use the new makefiles.

--- .buildbot.sh
+++ .buildbot.sh
@@ -1,23 +1,45 @@
 #!/bin/bash
 # vim: expandtab sts=0 sw=4 smarttab
+#
+# Copyright (c) 2020-2021 The CapableVMs "CHERI Examples" Contributors.
+# SPDX-License-Identifier: MIT OR Apache-2.0
+
 set -e
 
-find . -iname "*.c" -o -iname "*.h" -o -iname "*.cpp" -o -iname "*.hpp" | xargs ~/cheri/output/sdk/bin/clang-format --dry-run -Werror
+export CHERIBASE=~/cheri
+CHERIBUILD=~/build
 
-mkdir -p ./bin
-for example in *.c; do
-    if [ "${example}" == "seal.c" ]; then
-        continue
-    fi
-    make -f Makefile.riscv64 bin/"${example%%.*}"
+echo "Checking clang-format..."
+# Note that `sdk` and `morello-sdk` contain different versions of clang-format,
+# so we have to pick one and use it consistently.
+CLANG_FORMAT="$CHERIBASE"/output/sdk/bin/clang-format
+find . -iname "*.c" -o -iname "*.h" -o -iname "*.cpp" -o -iname "*.hpp" | xargs "$CLANG_FORMAT" --dry-run -Werror
+
+echo "Checking that all examples build for all platforms..."
+PLATFORMS='riscv64-purecap morello-purecap'
+# TODO: Add "example_allocators".
+for dir in . employee shared_objects timsort; do
+    pushd "$dir"
+    for platform in $PLATFORMS; do
+        make -f Makefile.$platform clean
+        if [ -d bin/$platform ]; then
+            echo "Error: 'make clean' did not clean the output directory."
+            false
+        fi
+        make -f Makefile.$platform all
+        if [ ! -d bin/$platform ]; then
+            echo "Error: 'make all' did not produce the expected output."
+            false
+        fi
+    done
+    popd
 done
 
-# Copy the test-script to `cheribuild`
-cp ./tests/run_cheri_examples.py $HOME/build/test-scripts/.
+export SSHPORT=10021
+export PYTHONPATH="$CHERIBUILD"/test-scripts
 
-# Run the tests on the `qemu-system riscv64`
+echo "Running tests for 'riscv64' using QEMU."
 args=(
-    # Architecture
     --architecture riscv64
     # Qemu System to use
     --qemu-cmd $HOME/cheri/output/sdk/bin/qemu-system-riscv64cheri
@@ -25,33 +47,12 @@ args=(
     --kernel $HOME/cheri/output/rootfs-riscv64-purecap/boot/kernel/kernel
     # Bios (to avoid the default one)
     --bios bbl-riscv64cheri-virt-fw_jump.bin
-    # Disk Image
     --disk-image $HOME/cheri/output/cheribsd-riscv64-purecap.img
     # Required build-dir in CheriBSD
     --build-dir .
-    --ssh-port 10021
+    --ssh-port $SSHPORT
     --ssh-key $HOME/.ssh/id_ed25519.pub
     )
+BUILDBOT_PLATFORM=riscv64-purecap python3 tests/run_cheri_examples.py "${args[@]}"
 
-python3 $HOME/build/test-scripts/run_cheri_examples.py "${args[@]}"
-rm -rfv bin/*
-
-BUILD_DIR="bin"
-
-# arg-1 : Source directory
-# arg-2 : cmake toolchain file
-function build_cheri_examples()
-{
-    ARCH=$(echo ${2} | cut -d '.' -f 1)
-    BUILD_DIR=${1}/build/${ARCH}
-
-    mkdir -p ${BUILD_DIR}
-    cmake -B ${BUILD_DIR} -S ${1}  -DCMAKE_TOOLCHAIN_FILE=${2}
-    cmake --build ${BUILD_DIR}
-}
-
-build_cheri_examples $(pwd) riscv64-purecap.cmake
-
-# FIXME: EXCLUDING MORELLO to close PR#26
-# build_cheri_examples $(pwd) morello-purecap.cmake
-rm -rfv $(pwd)/build
+# TODO: Run tests for Morello too.
