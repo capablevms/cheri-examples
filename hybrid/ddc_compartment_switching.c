8686cc66876c7ac591016e44a901d98aced4d088
Giuseppe De Ruvo <drgdr@pm.me>
2021-10-01 12:35:35 +0100

    Intermediate level of compartment creation via an assembly trampoline
    Given some allocated memory, e.g. 5000 B, we allow compartment_simple_fun access
    to the first 2000 Bytes and then we verify such a function cannot go over the bounds
    we set or that it can effectively access the data in its compartment.

---
+++ hybrid/ddc_compartment_switching.c
@@ -0,0 +1,36 @@
+/***
+ * TODO: something like intermediate level of compartment switching or ...
+ ***/
+
+#include "../include/common.h"
+#include "include/utils.h"
+#include <assert.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+
+#if !defined(__CHERI_CAPABILITY_WIDTH__) || defined(__CHERI_PURE_CAPABILITY__)
+#error "This example only works on CHERI hybrid mode"
+#endif
+
+// Simple function with some data which will be part of the compartment
+int compartment_simple_fun(int a, int b);
+// Temp hack to "design" an interface
+int switch_compartment(int a, int b, void * stack, size_t size);
+// TODO: this will call assembly code
+// extern int switch_compartment(int a, int b, void * stack, size_t size);
+
+int main(){
+    void * simple_block = malloc(5000);
+    size_t compartment_size = 2000;
+    switch_compartment(5, 3, simple_block, compartment_size);
+    return 0;
+}
+
+int switch_compartment(int a, int b, void * stack, size_t size){
+    return compartment_simple_fun(a, b); // FIXME: this is a temp hack
+}
+
+int compartment_simple_fun(int a, int b) {
+   return a + b;
+}
