a41b3e55231a43e96a0da7612f43668dd452f796
H Oliver <helen.oliver@bcs.org.uk>
2023-01-23 12:51:29 +0000

    shortened code, removed try_improve_publicreview, reduced can_read to one line, removed actual_input_length, removed review.h, added switch in CHERI code to turn off setting struct to read only so we can see it crash on overflow

--- compare_platforms/compare_platforms_read_only.c
+++ compare_platforms/compare_platforms_read_only.c
@@ -22,48 +22,88 @@
  *
  */
 
-#include "include/review.h"
+#ifdef __CHERI_PURE_CAPABILITY__
+#include "../include/common.h"
+#endif
+
 #include <assert.h>
+#include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+struct review
+{
+    char *username;
+    char *realname;
+    char *privatereview;
+    char *publicreview;
+    char *permissions;
+};
+
+bool can_write(struct review *rv) {
+    return (strchr(rv->permissions, 'w') != NULL);
+}
+
+bool change_publicreview(struct review *rv, char* newpublicreview, bool bWeak)
+{
+    if(can_write(rv) || (bWeak == false)) {
+        printf("\nAttempting to change the public review.\n");
+        rv->publicreview = newpublicreview;
+        return true;
+    }
+    else {
+        printf("\nYou don't have permission to change the public review.\n");
+        return false;
+    }
+}
+
+void print_details(struct review *rv)
+{
+    printf("\nReviewer Username: %s \n", rv->username);
+    printf("Reviewer's Real Name: %s \n", rv->realname);
+    printf("Private Review: %s\n", rv->privatereview);
+    printf("Public Review: %s\n", rv->publicreview);
+    printf("Review's Read-Write Permissions: %s\n", rv->permissions);
+    fflush(stdout);
+}
+
+struct review *set_read_only(struct review *rv)
+{
+    #ifdef __CHERI_PURE_CAPABILITY__
+        rv->username = (char *) cheri_perms_and(rv->username, CHERI_PERM_LOAD);
+        rv->realname = (char *) cheri_perms_and(rv->realname, CHERI_PERM_LOAD);
+        rv->privatereview = (char *) cheri_perms_and(rv->privatereview, CHERI_PERM_LOAD);
+        rv->publicreview = (char *) cheri_perms_and(rv->publicreview, CHERI_PERM_LOAD);
+        rv->permissions = (char *) cheri_perms_and(rv->permissions, CHERI_PERM_LOAD);
+        return (struct review *) cheri_perms_and(rv, CHERI_PERM_LOAD | CHERI_PERM_LOAD_CAP);
+    #else
+        assert(false);
+    #endif
+}
+
+
 int main()
 {
-    char *username, *realname, *privatereview, *publicreview, *permissions;
-    
-    // Our new review, which will have read only permissions.
     struct review review;
+    // In a non-CHERI environment, write protection is weakly enforced
+    bool bWeak = true;
     
     size_t smallsz = 0x20;
     size_t biggersz = 0x1001;
     
-    // Populate the members of the review struct.
-    username = malloc(smallsz);
+    char *username = malloc(smallsz);
     strcpy(username, "Reviewer 2");    
-    size_t szn = actual_input_length(username, smallsz);
-    
-    // Reviewer's real name
-    realname = malloc(smallsz);
+    char *realname = malloc(smallsz);
     strcpy(realname, "Baba Yaga");
-    size_t szp = actual_input_length(realname, smallsz);
-    
-    // The reviewer's private comments
-    privatereview = malloc(biggersz);
+    char *privatereview = malloc(biggersz);
     strcpy(privatereview, "I cannot believe I read this appalling piece of dreck from start to finish. The authors should be ashamed, and I hope I get an opportunity to tell them so to their faces.");
-    size_t szprr = actual_input_length(privatereview, biggersz);
-
-    // The public review
-    publicreview = malloc(biggersz);
+    char *publicreview = malloc(biggersz);
     strcpy(publicreview, "I am a little unclear as to the contribution. I think the authors could strengthen their case considerably if they conducted an RCT. Weak reject.");
-    size_t szpur = actual_input_length(publicreview, biggersz);
-    
-    // The permissions
-    permissions = malloc(smallsz);
+    char *permissions = malloc(smallsz);
     strcpy(permissions, "r");
-    size_t szpm = actual_input_length(permissions, smallsz);
     
     review.username = username;
     review.realname = realname;
@@ -83,21 +123,20 @@ int main()
     printf("publicreview=%#p permissions=%#p diff=%tx\n", publicreview, permissions, permissions - publicreview);
     printf("realname=%#p permissions=%#p diff=%tx\n", realname, permissions, permissions - realname);
 #else
-    printf("\nusername=%#p realname=%#p diff=%tx\n", username, realname, realname - username);
-    printf("realname=%#p privatereview=%#p diff=%tx\n", realname, privatereview, privatereview - realname);
-    printf("privatereview=%#p publicreview=%#p diff=%tx\n", privatereview, publicreview, publicreview - privatereview);
-    printf("publicreview=%#p permissions=%#p diff=%tx\n", publicreview, permissions, permissions - publicreview);
-    printf("realname=%#p permissions=%#p diff=%tx\n", realname, permissions, permissions - realname);
+    printf("\nusername=%p realname=%p diff=%tx\n", username, realname, realname - username);
+    printf("realname=%p privatereview=%p diff=%tx\n", realname, privatereview, privatereview - realname);
+    printf("privatereview=%p publicreview=%p diff=%tx\n", privatereview, publicreview, publicreview - privatereview);
+    printf("publicreview=%p permissions=%p diff=%tx\n", publicreview, permissions, permissions - publicreview);
+    printf("realname=%p permissions=%p diff=%tx\n", realname, permissions, permissions - realname);
 #endif
 
     assert((size_t)(username + smallsz) <= (size_t)realname); 
     assert((size_t)(privatereview + biggersz) <= (size_t)publicreview);
     assert((size_t)(realname + smallsz) <= (size_t)permissions); 
-
-// If we are in a CHERI environment, protect the review by setting
-// the relevant struct members (in this case all of them)
-// to read-only.  
-bool b_improved = false;
+  
+    bool b_improved = false;
+    char *newpublicreview = malloc(biggersz);
+    strcpy(newpublicreview, "5 ***** strong accept. This author deserves two Nobels and an ice cream.\n");
   
 #ifdef __CHERI_PURE_CAPABILITY__
     if(can_write(&review) == false) {
@@ -105,23 +144,27 @@ bool b_improved = false;
         struct review *ro_review = set_read_only(&review);
         assert((cheri_perms_get(ro_review) & CHERI_PERM_STORE) == 0);
 
-        printf("\nThe struct is read-only so trying to change the review will make the program crash...\n");
+        printf("\nThe struct is read-only so trying to change the review will make the program crash.\n");
+        printf("\nTry anyway? y/n: \n");
+        char answer;
+        scanf(" %c", &answer);
+        if (answer == 'Y' || answer == 'y'){
+            bWeak = false;
+            b_improved = change_publicreview(ro_review, newpublicreview, bWeak);
+            // This line should be unreachable.
+            print_details(ro_review);
+        }
         fflush(stdout);
-        
-        b_improved = try_improve_publicreview(ro_review, biggersz, false);
-        
-        // This line should be unreachable.
-        print_details(ro_review);
+        b_improved = false;
     }
 # else
-    // Try editing the public review in a non-CHERI environment,
-    // by simply checking the write permissions and refusing if none are found.
-    b_improved = try_improve_publicreview(&review, biggersz, false);
+    b_improved = change_publicreview(&review, newpublicreview, true);
 #endif
 
     // This code will be unreachable in a CHERI environment,
     // but it would crash whether or not the struct had been set to read-only.
     if(b_improved == false) {
+        
         printf("\nOverflowing reviewer realname by 1\n");
         memset(review.realname + smallsz+1, 'w', 1);
     
@@ -136,12 +179,18 @@ bool b_improved = false;
     
         print_details(&review);
         
-        b_improved = try_improve_publicreview(&review, biggersz, false);
+        b_improved = change_publicreview(&review, newpublicreview, bWeak);
         
         print_details(&review);
     }
 
     return 0;
 }
+
+
+
+
+
+
 
 
