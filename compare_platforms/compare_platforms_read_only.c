84d32d463865d0ff38a349e845ffbed0ea7cd48a
H Oliver <helen.oliver@bcs.org.uk>
2023-01-23 17:10:35 +0000

    Cleaned build setup, and restored all earlier changes: include/review.h removed and incorporated into compare_platforms_read_only.c; try_improve_public_review removed and functionality rolled into can_write; can_write reduced to a single line; conditional compilation moved inside the body of set_read_only and else clause added with an assert(false); switch added in CHERI code to set the struct to read-only or skip it in order to see the effects of the overflow in a CHERI environment.

--- compare_platforms/compare_platforms_read_only.c
+++ compare_platforms/compare_platforms_read_only.c
@@ -3,22 +3,29 @@
  * compare the two.
  * 
  * In a CHERI environment, it will:
- *     Step 1: Use can_read to check if the struct's "permissions" member includes write permissions.
+ *     Step 1: Use can_read to check if the struct's "permissions" member 
+includes write permissions.
  *     Step 2: Use capabilities to enforce read-only permissions
- *             (see https://github.com/capablevms/cheri-examples/blob/master/employee/include/employee.h).
+ *             (see 
+https://github.com/capablevms/cheri-examples/blob/master/employee/include/employee.h).
  *     Step 3: Try to write to the read-only struct members, and SIGPROT.
  *
  * In a non-CHERI environment, it will:
- *     Step 1: Use can_read to check if the struct's "permissions" member includes write permissions.
- *     Step 2: Try to exceed the bounds of an object and add write permissions
- *             (see https://ctsrd-cheri.github.io/cheri-exercises/exercises/buffer-overflow-heap/index.html).
+ *     Step 1: Use can_read to check if the struct's "permissions" member 
+includes write permissions.
+ *     Step 2: Try to exceed the bounds of an object and add write 
+permissions
+ *             (see 
+https://ctsrd-cheri.github.io/cheri-exercises/exercises/buffer-overflow-heap/index.html).
  *     Step 3: Rewrite the review.
  *
  * In a CHERI environment, the overflow attack will be unreachable.
- * In morello-purecap, it would crash immediately whether or not the struct
+ * In morello-purecap, it would crash immediately whether or not the 
+struct
  * had been set as read only.
  *
- * In a non-CHERI environment, this attack should succeed in corrupting some of the data.
+ * In a non-CHERI environment, this attack should succeed in corrupting 
+some of the data.
  *
  */
 
@@ -47,7 +54,8 @@ bool can_write(struct review *rv) {
     return (strchr(rv->permissions, 'w') != NULL);
 }
 
-bool change_publicreview(struct review *rv, char* newpublicreview, bool bWeak)
+bool change_publicreview(struct review *rv, char* newpublicreview, bool 
+bWeak)
 {
     if(can_write(rv) || (bWeak == false)) {
         printf("\nAttempting to change the public review.\n");
@@ -55,7 +63,8 @@ bool change_publicreview(struct review *rv, char* newpublicreview, bool bWeak)
         return true;
     }
     else {
-        printf("\nYou don't have permission to change the public review.\n");
+        printf("\nYou don't have permission to change the public 
+review.\n");
         return false;
     }
 }
@@ -73,12 +82,18 @@ void print_details(struct review *rv)
 struct review *set_read_only(struct review *rv)
 {
     #ifdef __CHERI_PURE_CAPABILITY__
-        rv->username = (char *) cheri_perms_and(rv->username, CHERI_PERM_LOAD);
-        rv->realname = (char *) cheri_perms_and(rv->realname, CHERI_PERM_LOAD);
-        rv->privatereview = (char *) cheri_perms_and(rv->privatereview, CHERI_PERM_LOAD);
-        rv->publicreview = (char *) cheri_perms_and(rv->publicreview, CHERI_PERM_LOAD);
-        rv->permissions = (char *) cheri_perms_and(rv->permissions, CHERI_PERM_LOAD);
-        return (struct review *) cheri_perms_and(rv, CHERI_PERM_LOAD | CHERI_PERM_LOAD_CAP);
+        rv->username = (char *) cheri_perms_and(rv->username, 
+CHERI_PERM_LOAD);
+        rv->realname = (char *) cheri_perms_and(rv->realname, 
+CHERI_PERM_LOAD);
+        rv->privatereview = (char *) cheri_perms_and(rv->privatereview, 
+CHERI_PERM_LOAD);
+        rv->publicreview = (char *) cheri_perms_and(rv->publicreview, 
+CHERI_PERM_LOAD);
+        rv->permissions = (char *) cheri_perms_and(rv->permissions, 
+CHERI_PERM_LOAD);
+        return (struct review *) cheri_perms_and(rv, CHERI_PERM_LOAD | 
+CHERI_PERM_LOAD_CAP);
     #else
         assert(false);
     #endif
@@ -99,9 +114,13 @@ int main()
     char *realname = malloc(smallsz);
     strcpy(realname, "Baba Yaga");
     char *privatereview = malloc(biggersz);
-    strcpy(privatereview, "I cannot believe I read this appalling piece of dreck from start to finish. The authors should be ashamed, and I hope I get an opportunity to tell them so to their faces.");
+    strcpy(privatereview, "I cannot believe I read this appalling piece of 
+dreck from start to finish. The authors should be ashamed, and I hope I 
+get an opportunity to tell them so to their faces.");
     char *publicreview = malloc(biggersz);
-    strcpy(publicreview, "I am a little unclear as to the contribution. I think the authors could strengthen their case considerably if they conducted an RCT. Weak reject.");
+    strcpy(publicreview, "I am a little unclear as to the contribution. I 
+think the authors could strengthen their case considerably if they 
+conducted an RCT. Weak reject.");
     char *permissions = malloc(smallsz);
     strcpy(permissions, "r");
     
@@ -117,17 +136,27 @@ int main()
         __builtin_cheri_round_representable_length(smallsz));
     printf("biggersz=%zx, CRRL(biggersz)=%zx\n", biggersz,
         __builtin_cheri_round_representable_length(biggersz));
-    printf("username=%#p realname=%#p diff=%tx\n", username, realname, realname - username);
-    printf("realname=%#p privatereview=%#p diff=%tx\n", realname, privatereview, privatereview - realname);
-    printf("privatereview=%#p publicreview=%#p diff=%tx\n", privatereview, publicreview, publicreview - privatereview);
-    printf("publicreview=%#p permissions=%#p diff=%tx\n", publicreview, permissions, permissions - publicreview);
-    printf("realname=%#p permissions=%#p diff=%tx\n", realname, permissions, permissions - realname);
+    printf("username=%#p realname=%#p diff=%tx\n", username, realname, 
+realname - username);
+    printf("realname=%#p privatereview=%#p diff=%tx\n", realname, 
+privatereview, privatereview - realname);
+    printf("privatereview=%#p publicreview=%#p diff=%tx\n", privatereview, 
+publicreview, publicreview - privatereview);
+    printf("publicreview=%#p permissions=%#p diff=%tx\n", publicreview, 
+permissions, permissions - publicreview);
+    printf("realname=%#p permissions=%#p diff=%tx\n", realname, 
+permissions, permissions - realname);
 #else
-    printf("\nusername=%p realname=%p diff=%tx\n", username, realname, realname - username);
-    printf("realname=%p privatereview=%p diff=%tx\n", realname, privatereview, privatereview - realname);
-    printf("privatereview=%p publicreview=%p diff=%tx\n", privatereview, publicreview, publicreview - privatereview);
-    printf("publicreview=%p permissions=%p diff=%tx\n", publicreview, permissions, permissions - publicreview);
-    printf("realname=%p permissions=%p diff=%tx\n", realname, permissions, permissions - realname);
+    printf("\nusername=%p realname=%p diff=%tx\n", username, realname, 
+realname - username);
+    printf("realname=%p privatereview=%p diff=%tx\n", realname, 
+privatereview, privatereview - realname);
+    printf("privatereview=%p publicreview=%p diff=%tx\n", privatereview, 
+publicreview, publicreview - privatereview);
+    printf("publicreview=%p permissions=%p diff=%tx\n", publicreview, 
+permissions, permissions - publicreview);
+    printf("realname=%p permissions=%p diff=%tx\n", realname, permissions, 
+permissions - realname);
 #endif
 
     assert((size_t)(username + smallsz) <= (size_t)realname); 
@@ -136,7 +165,8 @@ int main()
   
     bool b_improved = false;
     char *newpublicreview = malloc(biggersz);
-    strcpy(newpublicreview, "5 ***** strong accept. This author deserves two Nobels and an ice cream.\n");
+    strcpy(newpublicreview, "5 ***** strong accept. This author deserves 
+two Nobels and an ice cream.\n");
   
 #ifdef __CHERI_PURE_CAPABILITY__
     if(can_write(&review) == false) {
@@ -144,13 +174,15 @@ int main()
         struct review *ro_review = set_read_only(&review);
         assert((cheri_perms_get(ro_review) & CHERI_PERM_STORE) == 0);
 
-        printf("\nThe struct is read-only so trying to change the review will make the program crash.\n");
+        printf("\nThe struct is read-only so trying to change the review 
+will make the program crash.\n");
         printf("\nTry anyway? y/n: \n");
         char answer;
         scanf(" %c", &answer);
         if (answer == 'Y' || answer == 'y'){
             bWeak = false;
-            b_improved = change_publicreview(ro_review, newpublicreview, bWeak);
+            b_improved = change_publicreview(ro_review, newpublicreview, 
+bWeak);
             // This line should be unreachable.
             print_details(ro_review);
         }
@@ -162,18 +194,21 @@ int main()
 #endif
 
     // This code will be unreachable in a CHERI environment,
-    // but it would crash whether or not the struct had been set to read-only.
+    // but it would crash whether or not the struct had been set to 
+read-only.
     if(b_improved == false) {
         
         printf("\nOverflowing reviewer realname by 1\n");
         memset(review.realname + smallsz+1, 'w', 1);
     
-        // Now we can see the changes, if any. The permissions should have changed.
+        // Now we can see the changes, if any. The permissions should have 
+changed.
         print_details(&review);
     
         // Overflow the realname even more, which in a CHERI
         // environment should crash if it hasn't already.
-        const size_t oversz = review.permissions - review.realname + 2 - smallsz;
+        const size_t oversz = review.permissions - review.realname + 2 - 
+smallsz;
         printf("\nOverflowing reviewer realname by %zx\n", oversz);
         memset(review.realname + smallsz+2, 'w', oversz);
     
@@ -186,6 +221,7 @@ int main()
 
     return 0;
 }
+
 
 
 
