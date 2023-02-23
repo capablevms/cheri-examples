ec7adc9d74feaf73011fea60de2fbe9b18500c4a
H Oliver <helen.oliver@bcs.org.uk>
2023-02-23 18:43:08 +0000

    Simplified preamble and added comments

--- compare_platforms/compare_platforms_stacksmash.c
+++ compare_platforms/compare_platforms_stacksmash.c
@@ -1,44 +1,32 @@
 /*
+ * This example is meant to be run in multiple instances launched simultaneously.
+ * It can also be run as a single instance.
+ * 
  * Usage:
- * prompt> ./cpu_random & ./cpu_random & ./cpu_random & ./cpu_random &
- * 
- * This program declares two character pointers of equal length: 
- * first the Latin alphabet,
- * then an evil message that the user should not see.
- * 
- * The program then selects a substring from the alphabet, starting at a random index,
- * and displays it to the user 10 times.
+ * prompt> ./compare_platforms_stacksmash & ./compare_platforms_stacksmash & 
+ * or:
+ * prompt> ./compare_platforms_stacksmash
  *
- * Next, the program attempts to smash the stack:
- * It selects another random index that is certain to be out of bounds of the alphabet
- * pointer, tries to get a substring from this out-of-bounds index, and tries to display
- * this ill-gotten substring to the user.
- * 
- * The program is intended to be compiled and run in a CHERI or non-CHERI environment,
- * and the outputs compared. The points of comparison are:
- * - Does the program crash on the attempt to smash the stack?
- * - Does the user see the evil message that they should not see?
- * - When does the address of str change and when does it stay the same?
- * 
- * In a purecap environment, you should see the following:
- 
- * - A single instance of the program should crash; multiple instances should hang.
- * -- After the user manually kills one of the instances, the rest should follow without
- * user intervention.
- * - The user should not see the evil message.
- * - The capability should stay the same throughout, but the address of str should be
- * the same for all instances having the same value of str, and change when str's value
- * changes to the evil message. 
- * -- The value of str will usually, but not always, be the same for all instances.
- * -- The capability should stay the same throughout.
- * -- The address of the evil message will be outside the capability's bounds.
+ * The purpose of the example is to compare how a stack-smashing attempt is handled
+ * in a CHERI purecap vs hybrid environment, vs a non-CHERI environment.
  *
- * In morello-hybrid, you should see the following:
- * - The program should not crash or hang.
- * - The user should see the evil message.
- * - The address of str should be different for each respective instance, and should not
- * change when str's value changes to the evil message.
+ * The example first displays a substring of the Latin alphabet starting from a random index, 
+ * then tries to smash the stack using an index that is known to be out-of-bounds, 
+ * and display the out-of-bounds substring to the user.
  *
+ * In a CHERI purecap environment:
+ * - A single instance should crash, and multiple instances should hang;
+ * - The user should not see the out-of-bounds substring;
+ * - The address of the pointer should be the same for all instances but change when
+ * the substring's value changes.
+ * 
+ * In a morello-hybrid environment:
+ * - The program should run without crashing;
+ * - The user should see the out-of-bounds substring;
+ * - The address of the pointer should be different for all instances and should not 
+ * change when the substring's value changes. 
+ *
+ * In a non-CHERI environment, the program's behaviour is implementation dependent.
 */
 
 #include <assert.h>
@@ -67,7 +55,7 @@ void spin(intptr_t howlong)
 {
     intmax_t t = get_time();
     while ((get_time() - t) < (intmax_t) howlong)
-        ; // do nothing in loop
+        ; 
 }
 
 int main(int argc, char *argv[])
@@ -76,6 +64,7 @@ int main(int argc, char *argv[])
     char *evilmessage = "mine is an evil laugh muaha";
     srand((unsigned)time(NULL));
     
+    // get an index we know is within bounds of alphabet
     const size_t alphabet_size = 26;
     size_t goodindex = rand() % alphabet_size;
     char *str = &alphabet[goodindex];
@@ -88,14 +77,16 @@ int main(int argc, char *argv[])
     printf("Address of str = %p\n", &str);
 #endif
     
+    // Show the audience what they should be seeing
     for(int i=0; i<10; i++) {
         printf("%s\n", str);
         spin(1);
     }
 
+    // get an index we know is out-of-bounds of alphabet
     const size_t twice_alphabet_size = 52;
     size_t evilindex = twice_alphabet_size - goodindex;
-    //char *evilstr = &alphabet[evilindex];
+    // change the message to something the user shouldn't see
     str = &alphabet[evilindex];
     
 #ifdef __CHERI_PURE_CAPABILITY__
@@ -106,6 +97,7 @@ int main(int argc, char *argv[])
     printf("Address of str = %p\n", &str);
 #endif
 
+    // keep showing the ill-gotten string until the user kills the process
     while (1) {
         printf("%s\n", str);
         spin(1);
